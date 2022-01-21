cece6d6dbc815f6174f39c9f9e744f204e7dde66
unknown <anicetfosso@gmail.com>
2022-01-21 11:04:19 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import { StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Navigation from "./navigation";
+import { Block, Text } from "./components";
+
+ const App = () => {
+ 
+  const [isLoadingComplete, setIsLoadingComplete]=useState(false);
+
+  const _loadResourcesAsync = async () => {
+ 
+     await Font.loadAsync({
+        "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-Black": require("./assets/fonts/Rubik-Black.ttf"),
+        "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-Italic": require("./assets/fonts/Rubik-Italic.ttf"),
+        "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
+        "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf")
+      })
+
+    };
+
+    useEffect(() => {
+      _loadResourcesAsync
+    }, [])
+    const _handleLoadingError = (error) => {
+      // In this case, you might want to report the error to your error
+      // reporting service, for example Sentry
+      console.warn(error);
+    };
+  
+
+    return (
+      <Block>
+        <Navigation />
+      </Block>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
